ac7fccf53ec709d2bec32d08beefc447e479cfa4
JUHUI CHO <72244728+juhui88@users.noreply.github.com>
2023-12-29 14:06:06 +0900

    Title: 2개씩 그룹짓기, Time: 186ms, Memory: 25MB, Status: Passed - CodeTree

--- 231229/2개씩 그룹짓기/group-of-pairs.py
+++ 231229/2개씩 그룹짓기/group-of-pairs.py
@@ -1,5 +1,11 @@
 n = int(input())
 arr = list(map(int,input().split()))
 
+arr.sort()
+group_max = 0
+for i in range(n):
+    group_sum = arr[i] + arr[2*n-1-i]
+    if group_max <= group_sum:
+        group_max = group_sum
 
-print(arr[0] + arr[1])
+print(group_max)
